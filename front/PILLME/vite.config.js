b68749c7d0619b5929b6605874ad5fc39a2ea7f1
백지원 <olqkfh@naver.com>
2025-01-24 17:02:35 +0900

    fix:기존 전역 글로벌 css 삭제 및 테일윈드 4.0 적용

--- front/PILLME/vite.config.js
+++ front/PILLME/vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import { VitePWA } from 'vite-plugin-pwa';
-
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
       devOptions: {
