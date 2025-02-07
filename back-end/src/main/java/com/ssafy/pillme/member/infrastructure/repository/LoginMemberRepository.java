b392f3c68376d87d10f56a7d0762e740733e8fb7
EliteZer0 <diana.yy.jeong@gmail.com>
2025-02-07 16:59:48 +0900

    feat: LoginMember 레포지토리 인터페이스 구현
    - 중복 검사 메서드 추가
    - 삭제되지 않은 회원만 대상으로 검증

--- back-end/src/main/java/com/ssafy/pillme/member/infrastructure/repository/LoginMemberRepository.java
+++ back-end/src/main/java/com/ssafy/pillme/member/infrastructure/repository/LoginMemberRepository.java
@@ -1,4 +1,12 @@
 package com.ssafy.pillme.member.infrastructure.repository;
 
-public class LoginMemberRepository {
+import com.ssafy.pillme.member.domain.entity.LoginMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoginMemberRepository extends JpaRepository<LoginMember, Long> {
+    boolean existsByEmailAndDeletedFalse(String email);
+    boolean existsByPhoneAndDeletedFalse(String phone);
+    boolean existsByNicknameAndDeletedFalse(String nickname);
 }
