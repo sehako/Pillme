4db8b1119f51ad4da2ea506dce3a659f8f14c068
sehako <dhtpgkr1999@gmail.com>
2025-02-08 12:37:48 +0900

    fix: 엘라스틱 서치

--- back-end/src/main/java/com/ssafy/pillme/search/infrastructure/elasticsearch/MedicationElasticRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.pillme.search.infrastructure.elasticsearch;
-
-import com.ssafy.pillme.search.domain.elasticsearch.MedicationElastic;
-import java.util.Optional;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface MedicationElasticRepository extends ElasticsearchRepository<MedicationElastic, String> {
-    Optional<MedicationElastic> findByName(String name);
-}
