5ff7845dc89f091c67056eb0d3f398af4f598cd7
오세학 <dhtpgkr1999@naver.com>
2025-02-10 17:02:27 +0900

    test: cicd 테스트

--- back-end/src/main/java/com/ssafy/pillme/search/presentation/SearchController.java
+++ back-end/src/main/java/com/ssafy/pillme/search/presentation/SearchController.java
@@ -27,6 +27,6 @@ public class SearchController {
 
     @GetMapping("/{info-id}")
     public ResponseEntity<JSONResponse<String>> currentTakingDetail(@PathVariable("info-id") Long infoId) {
-        return ResponseEntity.ok(JSONResponse.onSuccess("cicd" + infoId));
+        return ResponseEntity.ok(JSONResponse.onSuccess("ㅂㅈㄷㅂㅈㄷㅂ" + infoId));
     }
 }
