c5ddd5280c2a65697e4ecf5d2f1a95b79999cf4b
임태훈 <xogns9647@naver.com>
2025-02-12 08:44:02 +0900

    refactor: param으로 파라미터 명시

--- back-end/src/main/java/com/ssafy/pillme/dependency/infrastructure/repository/DependencyRepository.java
+++ back-end/src/main/java/com/ssafy/pillme/dependency/infrastructure/repository/DependencyRepository.java
@@ -4,6 +4,7 @@ import com.ssafy.pillme.auth.domain.entity.Member;
 import com.ssafy.pillme.dependency.domain.entity.Dependency;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -21,7 +22,7 @@ public interface DependencyRepository extends JpaRepository<Dependency, Long> {
             "((d.protector.id = :firstMemberId AND d.dependent.id = :secondMemberId) OR " +
             "(d.protector.id = :secondMemberId AND d.dependent.id = :firstMemberId)) AND " +
             "d.deleted = false")
-    Optional<Dependency> findByMemberIdsAndDeletedIsFalse(Long firstMemberId, Long secondMemberId);
+    Optional<Dependency> findByMemberIdsAndDeletedIsFalse(@Param("firstMemberId") Long firstMemberId, @Param("secondMemberId") Long secondMemberId);
 
     // 회원의 모든 보호자 조회
     @Query("SELECT d.protector FROM Dependency d WHERE d.dependent = :dependent AND d.deleted = false")
@@ -30,7 +31,7 @@ public interface DependencyRepository extends JpaRepository<Dependency, Long> {
 
     // 피보호자와 보호자 id로 관계 조회
     @Query("SELECT d FROM Dependency d WHERE d.dependent.id = :dependentId AND d.protector.id = :protectorId AND d.deleted = false")
-    Optional<Dependency> findByDependentIdAndProtectorIdAndDeletedIsFalse(Long dependentId, Long protectorId);
+    Optional<Dependency> findByDependentIdAndProtectorIdAndDeletedIsFalse(@Param("dependentId") Long dependentId, @Param("protectorId") Long protectorId);
 
     // 회원 id로 모든 관계 조회
     @Query("SELECT d FROM Dependency d WHERE (d.protector.id = :memberId OR d.dependent.id = :memberId) AND d.deleted = false")
