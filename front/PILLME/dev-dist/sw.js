5290632d6c6dcebe1b20388038e41d20ee3e9f92
조승근 <wkfyddl1tp@naver.com>
2025-02-14 14:13:53 +0900

    fe/feat/chat

--- front/PILLME/dev-dist/sw.js
+++ front/PILLME/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.29ekc5ni8mo"
+    "revision": "0.v4lg3nkt0eg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
