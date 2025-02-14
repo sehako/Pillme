b9f906512b5098ea66d96a9a68ff1ecdd3d1c054
오세학 <dhtpgkr1999@naver.com>
2025-02-14 19:16:57 +0900

    test: 스케줄러 테스트

--- back-end/src/main/java/com/ssafy/pillme/history/application/HistoryScheduler.java
+++ back-end/src/main/java/com/ssafy/pillme/history/application/HistoryScheduler.java
@@ -1,11 +1,8 @@
 package com.ssafy.pillme.history.application;
 
-import com.ssafy.pillme.history.domain.History;
 import com.ssafy.pillme.history.infrastructure.HistoryRepository;
-import com.ssafy.pillme.management.domain.Management;
 import com.ssafy.pillme.management.infrastructure.ManagementRepository;
 import java.time.LocalDate;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -18,37 +15,38 @@ public class HistoryScheduler {
     private final HistoryRepository historyRepository;
     private final ManagementRepository managementRepository;
 
-    @Scheduled(cron = "0 0 4 * * *")
+    //    @Scheduled(cron = "0 0 2 * * *")
+    @Scheduled(cron = "0 * * * * *")
     public void creatHistory() {
         LocalDate validInformationDate = LocalDate.now().minusDays(1);
 
         log.info("{} 복약 내역 스케줄러 동작", validInformationDate);
-        List<Management> validManagements = managementRepository.findByInformationDate(validInformationDate);
-
-        List<History> histories = validManagements.stream()
-                .filter(management -> !management.isDeleted())
-                .map(management -> {
-                    History history = History.builder()
-                            .information(management.getInformation())
-                            .management(management)
-                            .member(management.getInformation().getReader())
-                            .morning(management.isMorning())
-                            .lunch(management.isLunch())
-                            .dinner(management.isDinner())
-                            .sleep(management.isSleep())
-                            .morningTaking(management.isMorningTaking())
-                            .lunchTaking(management.isLunchTaking())
-                            .dinnerTaking(management.isDinnerTaking())
-                            .sleepTaking(management.isSleepTaking())
-                            .takingDate(validInformationDate)
-                            .build();
-
-                    management.resetTakingInformation();
-                    return history;
-                })
-                .toList();
-
-        historyRepository.saveAll(histories);
+//        List<Management> validManagements = managementRepository.findByInformationDate(validInformationDate);
+//
+//        List<History> histories = validManagements.stream()
+//                .filter(management -> !management.isDeleted())
+//                .map(management -> {
+//                    History history = History.builder()
+//                            .information(management.getInformation())
+//                            .management(management)
+//                            .member(management.getInformation().getReader())
+//                            .morning(management.isMorning())
+//                            .lunch(management.isLunch())
+//                            .dinner(management.isDinner())
+//                            .sleep(management.isSleep())
+//                            .morningTaking(management.isMorningTaking())
+//                            .lunchTaking(management.isLunchTaking())
+//                            .dinnerTaking(management.isDinnerTaking())
+//                            .sleepTaking(management.isSleepTaking())
+//                            .takingDate(validInformationDate)
+//                            .build();
+//
+//                    management.resetTakingInformation();
+//                    return history;
+//                })
+//                .toList();
+//
+//        historyRepository.saveAll(histories);
 
     }
 }
