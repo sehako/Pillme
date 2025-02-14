06669e05bd9c09bfb36381572583b2d139eeac8e
백지원 <olqkfh@naver.com>
2025-02-14 21:17:28 +0900

    hotfix:  // 오늘의 복약 내역 불러오기   await fetchTodaysMedications();   await fetchData(); :await추가

--- front/PILLME/src/views/HomeView.vue
+++ front/PILLME/src/views/HomeView.vue
@@ -352,9 +352,10 @@ const completeMedications = async () => {
 
 //  컴포넌트가 마운트되면 데이터 및 이벤트 리스너 등록
 onMounted(async () => {
+  
   // 오늘의 복약 내역 불러오기
-  fetchTodaysMedications();
-  fetchData();
+  await fetchTodaysMedications();
+  await fetchData();
   // 알림 설정 불러오기
   await loadNotificationSettings();
 
