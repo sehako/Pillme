3b8ae3b8e9ed7168c32ec9583bff886d1f11fb5d
elitezer0 <diana.yy.jeong@gmail.com>
2025-02-17 00:14:43 +0900

    refactor: 리뷰 반영 get->post로 변경

--- back-end/src/main/java/com/ssafy/pillme/member/presentation/controller/ChangePasswordController.java
+++ back-end/src/main/java/com/ssafy/pillme/member/presentation/controller/ChangePasswordController.java
@@ -3,6 +3,7 @@ package com.ssafy.pillme.member.presentation.controller;
 import com.ssafy.pillme.global.response.JSONResponse;
 import com.ssafy.pillme.member.application.service.ChangePasswordService;
 import com.ssafy.pillme.member.presentation.request.ChangePasswordRequest;
+import com.ssafy.pillme.member.presentation.request.PasswordValidationRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,18 +14,19 @@ import org.springframework.web.bind.annotation.*;
 public class ChangePasswordController {
     private final ChangePasswordService changePasswordService;
 
-    // 비밀번호 변경
-    @GetMapping("/check/password")
-    public ResponseEntity<JSONResponse<Boolean>> validateNewPassword(
-            @RequestParam String newPassword) {
-        boolean isValid = changePasswordService.validateNewPasswordFormat(newPassword);
+    // 현재 로그인한 멤버 비밀번호 조회
+    @PostMapping("/password/verify")
+    public ResponseEntity<JSONResponse<Boolean>> checkCurrentPassword(
+            @RequestBody PasswordValidationRequest request) {
+        boolean isValid = changePasswordService.validateCurrentPassword(request.password());
         return ResponseEntity.ok(JSONResponse.onSuccess(isValid));
     }
 
-    // 현재 로그인한 멤버 비밀번호 조회
-    @GetMapping("/password")
-    public ResponseEntity<JSONResponse<Boolean>> checkCurrentPassword(@RequestParam String currentPassword) {
-        boolean isValid = changePasswordService.validateCurrentPassword(currentPassword);
+    // 새로운 비밀번호 유효성 검증
+    @PostMapping("/check/password")
+    public ResponseEntity<JSONResponse<Boolean>> validateNewPassword(
+            @RequestBody PasswordValidationRequest request) {
+        boolean isValid = changePasswordService.validateNewPasswordFormat(request.password());
         return ResponseEntity.ok(JSONResponse.onSuccess(isValid));
     }
 
