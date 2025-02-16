429ef259b5d7fda894f33db0714ce29fd5be5ec1
elitezer0 <diana.yy.jeong@gmail.com>
2025-02-16 14:02:17 +0900

    feat: 유저 에러메세지 구현

--- front/PILLME/src/views/PwSearchView.vue
+++ front/PILLME/src/views/PwSearchView.vue
@@ -7,24 +7,33 @@
     <BaseText highlightText="PILLME" />
 
     <div class="w-full max-w-xs md:max-w-sm space-y-4 mt-6">
+      <!-- 이메일 입력 -->
       <div class="flex flex-col w-full">
-        <BaseInput id="email" v-model="email" type="email" placeholder="회원가입 이메일" />
+        <BaseInput
+          id="email"
+          v-model="email"
+          type="email"
+          placeholder="회원가입 이메일"
+          class="w-full"
+        />
+        <p v-if="emailError" class="mt-1 text-red-500 text-sm">{{ emailError }}</p>
       </div>
 
+      <!-- 이메일 인증 버튼 -->
       <div class="flex flex-col w-full" v-if="!isEmailSent">
         <BaseButton
           @click="handleEmailVerification"
           textColor="text-white"
           size="sm"
-          :disabled="isLoading || isPhoneVerified"
-          overrideClass="!min-w-0 !w-auto !px-4 !py-2 !text-sm !bg-[#EF7C8E] group-hover:!bg-[#E96C7E] disabled:opacity-50"
+          :disabled="isLoading || isEmailVerified"
+          overrideClass="w-full !bg-[#EF7C8E] group-hover:!bg-[#E96C7E] disabled:opacity-50"
         >
           {{ isLoading ? '처리중...' : '인증번호 발송' }}
         </BaseButton>
       </div>
 
       <!-- 인증번호 입력 -->
-      <div class="flex flex-col w-full" v-if="isEmailSent">
+      <div class="flex flex-col w-full" v-if="isEmailSent && !isEmailVerified">
         <div class="flex items-center gap-2 group">
           <BaseInput
             id="authCode"
@@ -32,13 +41,12 @@
             type="text"
             placeholder="인증번호 입력"
             class="flex-grow"
-            :disabled="isPhoneVerified"
           />
           <BaseButton
             @click="handleVerifyCode"
             textColor="text-white"
             size="sm"
-            :disabled="isLoading || isPhoneVerified"
+            :disabled="isLoading"
             overrideClass="!min-w-0 !w-auto !px-4 !py-2 !text-sm !bg-[#EF7C8E] group-hover:!bg-[#E96C7E] disabled:opacity-50"
           >
             {{ isLoading ? '처리중...' : '확인' }}
@@ -46,22 +54,29 @@
         </div>
       </div>
 
-      <div class="flex flex-col w-full">
-        <div>
-          <BaseInput
-            id="phoneNumber"
-            v-model="phoneNumber"
-            type="tel"
-            placeholder="회원가입 전화번호"
-          />
-        </div>
+      <!-- 인증 완료 메시지 -->
+      <div v-if="isEmailVerified" class="w-full">
+        <p class="text-green-500 text-sm">인증번호 확인이 완료되었습니다.</p>
       </div>
 
+      <!-- 전화번호 입력 -->
+      <div class="flex flex-col w-full">
+        <BaseInput
+          id="phoneNumber"
+          v-model="phoneNumber"
+          type="tel"
+          placeholder="회원가입 전화번호"
+          class="w-full"
+        />
+      </div>
+
+      <!-- 임시 비밀번호 발송 버튼 -->
       <div class="flex justify-center">
         <BaseButton
           textColor="text-white"
           size="md"
-          overrideClass="!w-1/2 !px-4 !py-3.5 !text-sm !bg-[#EF7C8E] hover:!bg-[#E96C7E]"
+          :disabled="!isEmailVerified"
+          overrideClass="w-full !bg-[#EF7C8E] hover:!bg-[#E96C7E] disabled:opacity-50"
           @click="handleSubmit"
         >
           임시 비밀번호 발송
@@ -78,7 +93,11 @@
 <script setup>
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
-import { requestPasswordResetVerification, requestTemporaryPassword } from '../api/auth';
+import {
+  requestPasswordResetVerification,
+  requestTemporaryPassword,
+  verifyEmailCode,
+} from '../api/auth';
 import BaseButton from '../components/BaseButton.vue';
 import BaseInput from '../components/BaseInput.vue';
 import BaseLogo from '../components/BaseLogo.vue';
@@ -88,21 +107,25 @@ import logoSrc from '../assets/logi_nofont.svg';
 
 const router = useRouter();
 
-// 사용자 입력값
 const phoneNumber = ref('');
 const email = ref('');
 const isLoading = ref(false);
 const isEmailSent = ref(false);
 const authCode = ref('');
-const isPhoneVerified = ref(false);
+const isEmailVerified = ref(false);
+const emailError = ref('');
+const verificationSuccess = ref(false);
 
 const handleEmailVerification = async () => {
   try {
     isLoading.value = true;
+    emailError.value = '';
     await requestPasswordResetVerification(email.value);
     isEmailSent.value = true;
   } catch (error) {
     console.error('인증 메일 발송 실패:', error);
+    emailError.value = '인증번호 발송에 실패했습니다.';
+    isEmailSent.value = false;
   } finally {
     isLoading.value = false;
   }
@@ -111,9 +134,12 @@ const handleEmailVerification = async () => {
 const handleVerifyCode = async () => {
   try {
     isLoading.value = true;
-    isPhoneVerified.value = true;
+    await verifyEmailCode(email.value, authCode.value);
+    isEmailVerified.value = true;
+    isEmailSent.value = false;
   } catch (error) {
     console.error('인증 코드 인증 실패:', error);
+    emailError.value = '인증번호가 일치하지 않습니다.';
   } finally {
     isLoading.value = false;
   }
