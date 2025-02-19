f9a46690d98675d4223e3db0900a4a57cf4d6d75
오세학 <dhtpgkr1999@naver.com>
2025-02-19 13:07:09 +0900

    refactor: ToString

--- back-end/src/main/java/com/ssafy/pillme/management/domain/Information.java
+++ back-end/src/main/java/com/ssafy/pillme/management/domain/Information.java
@@ -16,13 +16,11 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Entity
 @Getter
 @Table(name = "information")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@ToString
 public class Information extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
