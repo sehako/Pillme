5b91ad67ba92e7547e7c2e329397695bea48e5f4
오세학 <dhtpgkr1999@naver.com>
2025-02-20 10:04:04 +0900

    fix: logout 로직 허용

--- back-end/src/main/java/com/ssafy/pillme/global/config/SecurityConfig.java
+++ back-end/src/main/java/com/ssafy/pillme/global/config/SecurityConfig.java
@@ -47,6 +47,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/v1/auth/signup",
                                 "/api/v1/auth/login",
+                                "/api/v1/auth/logout",
                                 "/api/v1/auth/email/verification",
                                 "/api/v1/auth/email/verify",
                                 "/api/v1/auth/sms/verification",
