cee019d466ad29154ac5baa90cd3edfac083caa7
BAEK JIWON <olqkfh@gmail.com>
2025-02-23 00:59:05 +0900

    fix:주석처리 안된 로그 주석처리5

--- front/PILLME/src/api/drugtaking.js
+++ front/PILLME/src/api/drugtaking.js
@@ -4,7 +4,7 @@ export const prescriptionAllCheck = async (medications, infoid) => {
   try {
     //`medicationsall` 같은 중첩된 키가 추가되지 않도록 올바르게 데이터 구조 확인
     // console.log("전송할 medications 데이터 구조:", Array.isArray(medications), medications);
-    console.log(infoid)
+    // console.log(infoid)
     const response = await apiClient.put(`/api/v1/management/${infoid}`, { medications });
 
     return response.data;
